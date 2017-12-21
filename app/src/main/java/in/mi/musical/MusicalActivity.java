d2cab8eb3b70baf9f53e37634a5e912859d7b588
mihir20 <mihir20121997@gmail.com>
2017-12-22 02:49:50 +0530

    initial commit

---
+++ app/src/main/java/in/mi/musical/MusicalActivity.java
@@ -0,0 +1,13 @@
+package in.mi.musical;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MusicalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_musical );
+    }
+}
